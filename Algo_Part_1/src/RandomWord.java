c34c7609046bdac83380df032deb1c709ff81863
Garvitjoshi1 <garvitjoshi46@gmail.com>
2024-04-10 22:28:12 +0530

    Java

---
+++ Algo_Part_1/src/RandomWord.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("My name is: "+str);
+    }
+}
